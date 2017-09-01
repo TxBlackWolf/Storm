d748bc40b7f29d3e000414776b8f5405b758cb91
Deathlive <Deathlive@users.noreply.github.com>
2017-09-01 21:23:46 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,23 +3,20 @@
 echo "Выберите Программу для установки"
 echo "1 Начальная Настройка"
 echo "2 Настройка Интерфейса"
-echo "3 Установить Metasploit Framework"
-echo "4 Установка Routersploit"
-echo "5 Дополнительные программы"
-echo "6 Выход"
+echo "3 Продолжить Интерфейса"
+echo "4 Установить Metasploit Framework"
+echo "5 Установка Routersploit"
+echo "6 Дополнительные программы"
+echo "7 Выход"
 read startgo
 
 case $startgo in
 1)
 echo "Начало настройки"
 cd $HOME
-pkg update
-pkg upgrade
+pkg update -y
+pkg upgrade -y
 pkg install -y python2 python2-dev python python-dev git nmap hydra nano ncdu proot fish zsh tsu coreutils wget tar bash clang curl libclang macchanger make man openssl openssl-dev
-cp -r /data/data/com.termux/files/home/Storm/prog/sqlmap /data/data/com.termux/files/home/ 
-echo "SQLmap установлена в каталог HOME"
-cp -r /data/data/com.termux/files/home/Storm/prog/responder /data/data/com.termux/files/home/ 
-echo "Responder установлена в каталог HOME"
 echo "Настройка завершена"
 ;;
 2)
@@ -28,21 +25,25 @@ cd $HOME
 echo PS1="\[\033[1;33;1;32m\]:\[\033[1;31m\]\w$ \[\033[0m\]\[\033[0m\]" > .bashrc
 chmod u+rwx,g+x .bashrc
 chsh -s fish
-rm -rf $HOME/.local/share/fish/
-cp -r $HOME/Storm/prog/fish/ $HOME/.local/share/
 echo "Настройка завершена, Fish Установлена по Умолчанияю"
-echo "Перезагрузите программу"
+echo "Перезагрузите программу CTR - D , Enter"
 ;;
 3)
-bash $HOME/Storm/script/msf.sh
+cd $HOME
+rm -rf $HOME/.local/share/fish/
+cp -r $HOME/Storm/prog/fish/ $HOME/.local/share/
+echo "Завершено"
 ;;
 4)
-bash $HOME/Storm/script/rsf.sh
+bash $HOME/Storm/script/msf.sh
 ;;
 5)
-bash $HOME/Storm/script/bonus.sh
+bash $HOME/Storm/script/rsf.sh
 ;;
 6)
+bash $HOME/Storm/script/bonus.sh
+;;
+7)
 exit 0
 ;;
 *)
