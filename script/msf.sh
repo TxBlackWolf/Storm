7ca56338523b30646d7db327a2461f1c609a9450
Deathlive <Deathlive@users.noreply.github.com>
2017-09-05 08:46:48 +0400

    Update msf.sh
    Translate in English language

--- script/msf.sh
+++ script/msf.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-echo "1 Начать установку Metasploit Framework"
-echo "2 Исправить ошибки rmf и datax"
-echo "3 Создать базу данных для MSF"
-echo "4 Выход"
+echo "1 Start installation Metasploit Framework"
+echo "2 Correct mistakes datax and PREFIX"
+echo "3 Create a database for MSF"
+echo "4 Exit"
 read msfgo
 
 case $msfgo in
@@ -14,13 +14,13 @@ apt upgrade
 apt install autoconf bison clang coreutils curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev \
     postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config \
     postgresql-contrib wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar
-echo "Зависимости Установлены"
+echo "Dependencies Installed"
 git clone https://github.com/rapid7/metasploit-framework
 cd $HOME/metasploit-framework/
 gem install bundler
 gem install nokogiri -- --use-system-libraries
 
-echo "Устанавливаем GRPC"
+echo "Install GRPC"
 cd $HOME/metasploit-framework/
 sed 's|grpc (.*|grpc (1.4.1)|g' -i Gemfile.lock
 gem unpack grpc -v 1.4.1
@@ -33,7 +33,7 @@ gem install grpc-1.4.1.gem
 cd ..
 rm -rf grpc-1.4.1
 
-echo "Устанавливаем rbnacl-libsodium"
+echo "Install rbnacl-libsodium"
 gem unpack rbnacl-libsodium -v'1.0.13'
 cd rbnacl-libsodium-1.0.13
 termux-fix-shebang ./vendor/libsodium/configure ./vendor/libsodium/build-aux/*
@@ -46,33 +46,33 @@ gem install rbnacl-libsodium-1.0.13.gem
 cd .. 
 rm -rf rbnacl-libsodium-1.0.13
 
-echo "Установка GEM"
+echo "Install GEM"
 cd $HOME/metasploit-framework
 bundle install -j5
 
 cd $HOME
 mv $HOME/metasploit-framework $HOME/msfconsole
-echo "msfconsole установлена, начние пункт 2"
+echo "msfconsole installed, Start the option-2"
 ;;
 2)
-echo "Исправляем ощибку rmf и datax"
+echo "Correct mistakes datax and Shebang-PREFIX"
 cd $HOME
 pkg install -y termux-elf-cleaner
 termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
-echo "Datax ошибка устранена"
+echo "Datax error fixed"
 
-echo "Выполнение shebang fix"
+echo "Performance shebang fix"
 cd $HOME/metasploit-framework
 $PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
-echo "Установка завершена"
+echo "Installation completed"
 ;;
 3)
-echo "Создаем базу данных"
+echo "Create a database"
 cd $HOME
 mkdir ~/.msfdb
 initdb ~/.msfdb
 pg_ctl -D ~/.msfdb -l ~/.msfdb/msfdb.log start
-echo "Старт сервера"
+echo "Server start"
 echo "createuser msf and createdb msfdb"
 echo "pg_ctl -D ~/.msfdb -l ~/.msfdb/msfdb.log stop"
 ;;
@@ -80,6 +80,6 @@ echo "pg_ctl -D ~/.msfdb -l ~/.msfdb/msfdb.log stop"
 exit 0
 ;;
 *)
-echo "Выберите правильный вариант"
+echo "Choose the correct option"
 ;;
 esac
