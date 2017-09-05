d2578860e096632ae1928033e4fa7cf5ea23b4ec
Deathlive <Deathlive@users.noreply.github.com>
2017-09-05 08:42:19 +0400

    Update bonus.sh
    Translate the English language

--- script/bonus.sh
+++ script/bonus.sh
@@ -1,31 +1,31 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-echo "1 Начать установку Reaver"
-echo "2 Начать установку Dnschef"
-echo "3 Начать установку Sqlmap"
-echo "4 Начать установку Responder"
-echo "5 Выход"
+echo "1 Start installation Reaver"
+echo "2 Start installation Dnschef"
+echo "3 Start installation Sqlmap"
+echo "4 Start installation Responder"
+echo "5 Exit"
 read bonus
 
 case $bonus in
 1)
-echo "Начало Установки Reaver"
+echo "Start install Reaver"
 cd $HOME
 git clone https://github.com/Deathlive/reaver
 mv $HOME/reaver-wps-fork-t6x $HOME/reaver
 cd $HOME/reaver/src/
 sh configure
 make
-echo "Завершение Установки"
+echo "End of Setup"
 ;;
 2)
-echo "Начало Установки Dnschef"
+echo "Start install Dnschef"
 cd $HOME
 pkg install dnsutils
 pip2 install dnslib ipy
 git clone https://github.com/iphelix/dnschef
 cd $HOME/dmschef/
-echo "Завершение Установки, python2 dnschef.py"
+echo "End of Setup, python2 dnschef.py"
 ;;
 3)
 cd $HOME
@@ -38,6 +38,6 @@ git clone https://github.com/SpiderLabs/Responder
 cd $HOME/Responder/
 ;;
 *)
-echo "Выберите правильный вариант"
+echo "Choose the correct option"
 ;;
 esac
