ad28069cf8f29c9bc761abf071e8b55383a43042
Deathlive <Deathlive@users.noreply.github.com>
2017-09-05 08:40:01 +0400

    Update rsf.sh
    Translate in English language.

--- script/rsf.sh
+++ script/rsf.sh
@@ -1,8 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-echo "Начало Установки RouterSploit \n"
+echo "Start Install RouterSploit \n"
 cd $HOME
 git clone https://github.com/reverse-shell/routersploit
 cd $HOME
 pip2 install six pycparser cffi
-echo "Дальше сами, Termux-chroot, cd routersploit, pip2 install -r requirements.txt"
+echo "------------------"
+echo "Termux-chroot, cd routersploit, pip2 install -r requirements.txt"
+echo "------------------"
