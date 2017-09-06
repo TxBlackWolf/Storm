b6ef8b8a3885154e02d8c821988c7eeff69143fc
Deathlive <Deathlive@users.noreply.github.com>
2017-09-06 11:46:16 +0400

    Add MITMf

--- script/bonus.sh
+++ script/bonus.sh
@@ -4,7 +4,8 @@ echo "1 Start installation Reaver"
 echo "2 Start installation Dnschef"
 echo "3 Start installation Sqlmap"
 echo "4 Start installation Responder"
-echo "5 Exit"
+echo "5 Start installation MITMf"
+echo "6 Exit"
 read bonus
 
 case $bonus in
@@ -35,6 +36,17 @@ cd $HOME/sqlmap/
 cd $HOME
 git clone https://github.com/SpiderLabs/Responder
 cd $HOME/Responder/
+;;
+4)
+cd $HOME
+git clone https://github.com/byt3bl33d3r/MITMf
+pkg install clang python2 python2-dev libjpeg-turbo-dev ndk-sysroot clang
+pip2 install wheel Twisted
+LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install pillow
+echo "termux-chroot, pip2 install -r requirements.txt
+;;
+6)
+exit 0
 ;;
 *)
 echo "Choose the correct option"
