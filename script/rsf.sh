33f295b1e580ad25f86e3cb53be4ec7cdf9e0815
Deathlive <Deathlive@users.noreply.github.com>
2017-09-18 08:56:02 +0400

    Update rsf.sh

--- script/rsf.sh
+++ script/rsf.sh
@@ -4,7 +4,7 @@ echo "Start Install RouterSploit"
 cd $HOME
 git clone https://github.com/reverse-shell/routersploit
 cd $HOME
-pip2 install six pycparser cffi requests
+pip2 install six wheel pycparser cffi requests
 SODIUM_INSTALL=system pip2 install pynacl
 echo "------------------"
 echo "Termux-chroot, cd routersploit, pip2 install -r requirements.txt"
