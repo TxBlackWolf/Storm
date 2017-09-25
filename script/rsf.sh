0432d942af14e370a386c600f489e277f3ce5ca7
Deathlive <Deathlive@users.noreply.github.com>
2017-09-25 10:20:37 +0400

    Update rsf.sh

--- script/rsf.sh
+++ script/rsf.sh
@@ -6,8 +6,8 @@ git clone https://github.com/reverse-shell/routersploit
 cd $HOME
 pip2 install six wheel pycparser cffi requests
 SODIUM_INSTALL=system pip2 install pynacl
-pkg install libjpeg-turbo-dev
-$ LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
+#pkg install libjpeg-turbo-dev
+#LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
 echo "------------------"
 echo "use termux-chroot, cd routersploit, pip2 install -r requirements.txt"
 echo "------------------"
