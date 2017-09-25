ef1777334220f923ccd6c8a1880606cbad57a7e2
Deathlive <Deathlive@users.noreply.github.com>
2017-09-25 19:07:58 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/sh
-
-echo "Select the Program to install"
+echo "   _____   _______    ____    _____    __  __  "
+echo "  / ____| |__   __|  / __ \  |  __ \  |  \/  | "
+echp " | (___      | |    | |  | | | |__) | | \  / | "
+echo "  \___ \     | |    | |  | | |  _  /  | |\/| | "
+echo "  ____) |    | |    | |__| | | | \ \  | |  | | "
+echo " |_____/     |_|     \____/  |_|  \_\ |_|  |_| "
+                                              
 echo "1 Primary setting"
 echo "2 Install the Metasploit Framework"
 echo "3 Install the Routersploit"
