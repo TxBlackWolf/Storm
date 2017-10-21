4874adeedeed384ca8f627fe8e588e9bd42bf613
Deathlive <Deathlive@users.noreply.github.com>
2017-10-21 15:56:01 +0400

    Update rsf.sh

--- script/rsf.sh
+++ script/rsf.sh
@@ -4,10 +4,10 @@ echo "Start Install RouterSploit"
 cd $HOME
 git clone https://github.com/reverse-shell/routersploit
 cd $HOME
-pkg install python2 python2-dev python python-dev coreutils clang libclang libsodium libsodium-dev libtool libffi libffi-dev libgrpc-dev readline-dev
+apt install python2 python2-dev python python-dev coreutils clang libclang libsodium libsodium-dev libtool libffi libffi-dev libgrpc-dev readline-dev
 pip2 install six wheel pycparser cffi requests
 SODIUM_INSTALL=system pip2 install pynacl
-#pkg install libjpeg-turbo-dev
+#apt install libjpeg-turbo-dev
 #LDFLAGS="-L/system/lib/" CFLAGS="-I/data/data/com.termux/files/usr/include/" pip install Pillow
 echo "------------------"
 echo "use termux-chroot, cd routersploit, pip2 install -r requirements.txt"
