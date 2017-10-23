69b01113e511d3d66b612bd0dd4b38d5dc14b19f
Deathlive <Deathlive@users.noreply.github.com>
2017-10-23 17:04:42 +0400

    Debian add

--- start.sh
+++ start.sh
@@ -7,11 +7,12 @@ echo "  ____) |    | |    | |__| | | | \ \  | |  | | "
 echo " |_____/     |_|     \____/  |_|  \_\ |_|  |_| "
                                               
 echo "1 Primary setting"
-echo "2 Install the Metasploit Framework"
-echo "3 Install the Routersploit"
-echo "4 Programs list-1"
-echo "5 Programs list-2"
-echo "6 Exit"
+echo "2 Debian Install"
+echo "3 Install the Metasploit Framework"
+echo "4 Install the Routersploit"
+echo "5 Programs list-1"
+echo "6 Programs list-2"
+echo "7 Exit"
 read startgo
 
 case $startgo in
@@ -19,18 +20,21 @@ case $startgo in
 bash $HOME/Storm/script/primaryset.sh
 ;;
 2)
-bash $HOME/Storm/script/msf.sh
+bash $HOME/Storm/script/debian.sh
 ;;
 3)
-bash $HOME/Storm/script/rsf.sh
+bash $HOME/Storm/script/msf.sh
 ;;
 4)
-bash $HOME/Storm/script/programlist_1.sh
+bash $HOME/Storm/script/rsf.sh
 ;;
 5)
-bash $HOME/Storm/script/programlist_2.sh
+bash $HOME/Storm/script/programlist_1.sh
 ;;
 6)
+bash $HOME/Storm/script/programlist_2.sh
+;;
+7)
 exit 0
 ;;
 *)
